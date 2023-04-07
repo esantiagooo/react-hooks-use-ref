1b7badcefbaae40cedaacf5a06959e8ddddffb86
Eric <eric.yanez50@qmail.cuny.edu>
2023-04-07 14:59:13 -0400

    done

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,17 @@
 import React, { useRef } from "react";
 
-function Box() {
+const Box = () => {
   const elementRef = useRef();
+  
+  const handleClickRef = () => {
+    const div = elementRef.current
+    console.log('Measurements:', div.getBoundingClientRect())
+  }
 
   return (
     <div ref={elementRef}>
       <h1>Box</h1>
-      <button>Measure</button>
+      <button onClick={handleClickRef}>Measure</button>
     </div>
   );
 }
